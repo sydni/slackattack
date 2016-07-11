6592985f85f558704c03bd5453f9c65a2a6a6007
Sydni Topper <sydni.topper.18@dartmouth.edu>
2016-07-10 20:20:16 -0400

    bot can have a conversation and some yelp stuff

--- app/server.js
+++ app/server.js
@@ -1,11 +1,21 @@
-// example bot
 import botkit from 'botkit';
+
 // this is es6 syntax for importing libraries
 // in older js this would be: var botkit = require('botkit')
 
 console.log('starting bot');
 
+import Yelp from 'yelp';
 
+const yelp = new Yelp({
+  consumer_key: 'Mm9JyZAXvfGL4uQzVCoC1Q',
+  consumer_secret: 'AnPxMHKpZYdduI3XhdP0TtDdr_w',
+  token: 'nRxYF0X0vjU8qSlqcUEo_Syh_ZXXJ_vk',
+  token_secret: '88wbOCX2jOW7iKjL5WIyKjNDPRc',
+});
+// $.ajax({
+//   url: ''
+// })
 // botkit controller
 const controller = botkit.slackbot({
   debug: false,
@@ -28,7 +38,151 @@ controller.setupWebserver(process.env.PORT || 3001, (err, webserver) => {
   });
 });
 
-// example hello response
 controller.hears(['hello', 'hi', 'howdy'], ['direct_message', 'direct_mention', 'mention'], (bot, message) => {
-  bot.reply(message, 'Hello there!');
+  bot.api.users.info({ user: message.user }, (err, res) => {
+    if (res) {
+      bot.reply(message, `Hello, ${res.user.profile.first_name}!`);
+    } else {
+      bot.reply(message, 'Hello there!');
+    }
+  });
+});
+
+
+controller.hears(['help'], ['direct_message', 'direct_mention', 'mention'], (bot, message) => {
+  bot.reply(message, 'Hello, I am hot_bot! I can help you look for food near you, or ask you questions!');
+});
+
+
+// adapted from https://github.com/howdyai/botkit
+controller.hears(['ask me', 'question'], ['direct_message', 'direct_mention', 'mention'], (bot, message) => {
+  // start a conversation to handle this response.
+  bot.startConversation(message, (err, convo) => {
+    convo.ask('What do you think the meaning of life is?', [
+      {
+        pattern: '42',
+        callback: (response, convo) => {
+          convo.say('What a creative answer');
+          convo.ask('Do you read a lot?', [
+            {
+              pattern: bot.utterances.yes,
+              callback: (response, convo) => {
+                convo.say('Cool me too!');
+                convo.ask('What is your favorite book?', (book, convo) => {
+                  convo.say(`${book.text} is a great book. Talk to you later!`);
+                  convo.next();
+                });
+                convo.next();
+              },
+            },
+            {
+              pattern: bot.utterances.no,
+              callback: (response, convo) => {
+                convo.say('You should read more. Talk to you later!');
+                convo.next();
+              },
+            },
+          ]);
+          convo.next();
+        },
+      },
+      {
+        pattern: 'food',
+        callback(response, convo) {
+          convo.say('We should catch up and get a meal sometime!');
+          convo.ask('Are you free tomorrow?', [
+            {
+              pattern: bot.utterances.yes,
+              callback: (response, convo) => {
+                convo.ask('What time are you free?', (time, convo) => {
+                  convo.say(`Great! Let's meet at Foco at ${time.text}. See you then!`);
+                  convo.next();
+                });
+                convo.next();
+              },
+            },
+            {
+              pattern: bot.utterances.no,
+              callback: (response, convo) => {
+                convo.say('Ouch.');
+                convo.next();
+              },
+            },
+            {
+              default: true,
+              callback: (response, convo) => {
+                convo.say('I don\'t understand.');
+                convo.repeat();
+                convo.next();
+              },
+            },
+          ]);
+          convo.next();
+          convo.next();
+        },
+      },
+      {
+        pattern: 'fun',
+        callback(response, convo) {
+          convo.say('You seem like a cool dude.');
+          convo.next();
+        },
+      },
+      {
+        default: true,
+        callback(response, convo) {
+          convo.say('Wrong.');
+          convo.repeat();
+          convo.next();
+        },
+      },
+    ]);
+  });
+});
+
+
+controller.hears(['food', 'hungry'], ['direct_message', 'direct_mention', 'mention'], (bot, message) => {
+  bot.startConversation(message, (response, con) => {
+    con.ask('What do you want to eat?', (food, conv) => {
+      conv.say(`You want ${food.text}?`); // figureout how to concatenate strings
+      conv.ask('Where are you?', (location, convo) => {
+        // cite this later
+
+        yelp.search({ term: food.text, location: location.text, limit: 1 })
+        .then((data) => {
+          convo.say(`Finding results near ${location.text}`);
+          convo.next();
+          // console.log(data);
+
+          const bname = data.businesses.map(post => {
+            convo.say(post.name);
+            convo.next();
+            return post.name;
+          });
+
+          console.log(bname);
+          convo.say('test');
+
+          // convo.next();
+          // convo.next();
+
+          // convo.say('TESTTEST');
+          // convo.say(data.total);
+          // convo.next();
+        })
+        .catch((err) => {
+          console.error(err);
+        });
+
+
+        convo.next();
+      });
+      conv.next();
+    });
+  });
+});
+
+
+controller.on('direct_message', (bot, message) => {
+  bot.reply(message, 'What are you even talking about?');
 });
